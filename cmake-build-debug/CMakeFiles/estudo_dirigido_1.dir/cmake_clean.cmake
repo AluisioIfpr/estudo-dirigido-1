a34082c1ba28b04d4b9b9b46624dfa991085d505
jefferson.chaves <jeffersong.s@hotmail.com>
2019-09-12 11:07:30 -0300

    Sinc repo

---
+++ cmake-build-debug/CMakeFiles/estudo_dirigido_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/estudo_dirigido_1.dir/main.cpp.obj"
+  "estudo_dirigido_1.pdb"
+  "estudo_dirigido_1.exe"
+  "estudo_dirigido_1.exe.manifest"
+  "libestudo_dirigido_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/estudo_dirigido_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/estudo_dirigido_1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for estudo_dirigido_1.
+# This may be replaced when dependencies are built.
